b00a11755d8b5212ae0e52c9d85a6b08989d9c5d
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-02-23 18:29:27 +0000

    //tracker.nci.nih.gov/browse/OCEPROJECT-4403 - Added temporary ClientUtils.cs - Updated login.aspx.cs to use updated methods

--- PERT/login.aspx.cs
+++ PERT/login.aspx.cs
@@ -7,9 +7,6 @@ using System.Web.UI.WebControls;
 
 using PubEnt.DAL;
 using PubEnt.BLL;
-using Aspensys.GlobalUsers.WebServiceClient;
-using Aspensys.GlobalUsers.WebServiceClient.UserService;
-
 
 namespace PubEnt
 {
@@ -69,29 +66,34 @@ namespace PubEnt
             {
                 try
                 {
-
+                    /*
                     new UserServiceClient().Using(client =>
-                    {
-                        bool bIsUserValid = (bool)client.ValidateUser(username, password).ReturnValue;
-                       
+                    {*/
+                    ClientUtils client = new ClientUtils();
+                    bool bIsUserValid = client.ValidateUser(username, password);
 
                         if (bIsUserValid)
                         {
                             Session["NCIPL_User"] = txtUserName.Text;
 
-                            var user_roles = client.GetRolesForUser(txtUserName.Text.ToString()).ReturnValue as string[];
-
-
-                           
-
+                            // Get list of user roles
+                            var user_roles = client.GetRolesForUser(txtUserName.Text.ToString());
                             if (user_roles != null && user_roles.Count() != 0)
                             {
                                 Session["NCIPL_Role"] = user_roles[0];
+                                // Iterate through list of roles and set role value 
+                                foreach (string role in user_roles)
+                                {
+                                    if (role.ToUpper() == "NCIPL_LM" || role.ToUpper() == "NCIPL_PO")
+                                    {
+                                        Session["NCIPL_Role"] = role;
+                                        break;
+                                    }
+                                }
                             }
 
                             if (GlobalUtils.UserRoles.getLoggedInUserId().Length == 0 || GlobalUtils.UserRoles.getLoggedInUserRole() < 1)
                             {
-                                
                                 lblGuamMsg.Text = "Invalid User or Role.";
                                 lblGuamMsg.Visible = true;
                             }
@@ -102,11 +104,11 @@ namespace PubEnt
                             }
 
                             //yma add this maximum password age 60days rule
-                            if ((bool)client.GetMustChangePasswordFlag(username).ReturnValue)
+                            if (client.GetMustChangePasswordFlag(username) == true)
                             {
                                 Response.Redirect("changepwd.aspx");
                             }
-                            else if ((bool)client.IsPasswordExpired(username).ReturnValue)
+                            else if (client.IsPasswordExpired(username))
                             {
                                 Response.Redirect("changepwd.aspx");
                             }
@@ -124,11 +126,23 @@ namespace PubEnt
                         }
                         else
                         {
-                            ReturnObject ro = client.GetValidationFailureReason(username);
-                            lblGuamMsg.Text = ro.DefaultErrorMessage;
+                            //do not give auth ticket
+                            //ReturnObject ro = client.GetValidationFailureReason(username);
+                            int ro = client.GetValidationFailureReason(username, password);
+
+                            //yma add this to display customized msg
+                            if (ro == 106)
+                            {
+                                lblGuamMsg.Text = "This account is disabled. Please email testuser1@pubs.cancer.gov for help.";
+                            }
+                            else
+                            {
+                                //display failure code on login screen
+                                lblGuamMsg.Text = "Incorrect username and/or password. Please try again.";
+                            }
                             lblGuamMsg.Visible = true;
                         }
-                    });
+                    //});
                 }
                 catch//(Exception ex)
                 {
