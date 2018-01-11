d9d303899761fe87d29f829bb5efaf487b349e87
Dion <daquinohd@users.noreply.github.com>
2018-01-11 16:28:08 -0500

    Created local build batch file / renamed XML to avoid confusion

---
+++ tools/build/BuildPubsLocator-local.bat
@@ -0,0 +1,11 @@
+@echo off
+SETLOCAL
+
+@set PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files\MSBuild\Microsoft\Windows Workflow Foundation\v3.5;C:\Program Files (x86)\Microsoft.NET\RedistList;%PATH%
+@set LIBPATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;%LIBPATH%
+
+ECHO Building PubsLocator
+ECHO Todo: fix msbuild.log write permissions
+msbuild /verbosity:detailed /target:ALL BuildPubsLocator.xml
+ECHO Done building PubsLocator hashtaguniquecomment
+pause
