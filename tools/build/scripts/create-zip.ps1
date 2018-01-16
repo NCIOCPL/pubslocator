2a9dcf08abd8b43fb6e2e2c1b4bcb7e9b5448414
Dion <daquinohd@users.noreply.github.com>
2018-01-16 16:26:32 -0500

    Rolled everything back -- again

--- tools/build/scripts/create-zip.ps1
+++ tools/build/scripts/create-zip.ps1
@@ -23,8 +23,4 @@ Param(
 #>
 
 Write-Host "Creating '${destinationPath}' from '${sourcePath}'."
-$source = ${sourcePath}
-$archive = ${destinationPath}
-Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::CreateFromDirectory($source, $archive)
-<# https://stackoverflow.com/questions/40692024/zip-and-unzip-file-in-powershell-4 #>
+Compress-Archive -Path $sourcePath -DestinationPath $destinationPath
