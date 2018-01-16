ba1db100d566fa9689d7324fa4e6618a02b53a16
Dion <daquinohd@users.noreply.github.com>
2018-01-16 16:22:20 -0500

    Trying to use ps4-safe functions

--- tools/build/scripts/create-zip.ps1
+++ tools/build/scripts/create-zip.ps1
@@ -23,4 +23,8 @@ Param(
 #>
 
 Write-Host "Creating '${destinationPath}' from '${sourcePath}'."
-Compress-Archive -Path $sourcePath -DestinationPath $destinationPath
+$source = ${sourcePath}
+$archive = ${destinationPath}
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($source, $archive)
+<# https://stackoverflow.com/questions/40692024/zip-and-unzip-file-in-powershell-4 #>
