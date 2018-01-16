72ec756968b948f1b1b6ccb71275eb335e1db80a
Dion <daquinohd@users.noreply.github.com>
2018-01-16 14:59:42 -0500

    Added environment variables to deploy,bat

--- tools/build/BuildPubsLocator.bat
+++ tools/build/BuildPubsLocator.bat
@@ -4,12 +4,27 @@ SETLOCAL
 @set PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files\MSBuild\Microsoft\Windows Workflow Foundation\v3.5;C:\Program Files (x86)\Microsoft.NET\RedistList;%PATH%
 @set LIBPATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;%LIBPATH%
 
-ECHO Building PubsLocator
-ECHO Todo: fix msbuild.log write permissions
+REM Check environment variables
+set FAIL=
+IF "%WORKSPACE%"=="" set FAIL=True
+IF "%TEMP%"=="" set FAIL=True
+IF "%BUILD_NUMBER%"=="" set FAIL=True
+IF "%FAIL%" NEQ "" (
+	ECHO Fail. Fix your stuff.
+	GOTO :EOF
+)
+
+REM Determine the current Git commit hash.
+FOR /f %%a IN ('git rev-parse --verify HEAD') DO SET COMMIT_ID=%%a
+
+REM Do the build
+REM Todo: fix msbuild.log write permissions
+ECHO Building PubsLocator...
 msbuild /verbosity:detailed /target:ALL "%WORKSPACE%\tools\build\BuildPubsLocatorCode.xml"
-ECHO Done building PubsLocator hashtaguniquecomment
+ECHO Completed building PubsLocator 
 
 REM Set environment variables
 SET WORKSPACE=%WORKSPACE%
+SET GH_REPO_NAME=%GH_REPO_NAME%
 
-pause
+PAUSE
